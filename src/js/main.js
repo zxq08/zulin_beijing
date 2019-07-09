edb88520b4df6c91e5d2fc565bd68130ca6980a6
zxq <461890276@qq.com>
2019-07-09 23:23:49 +0800

    2019/07/09 zxq 第二次提交

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-	$('#tabs_button').on('click', function (e) {
-		$('#tabs_button > li > a').toggleClass('active',false);
-		$(e.target).addClass('active');
-	});
+
 	$('#logOut').on('click', function () {
 		$(location).attr('href','./login.html');
 	});
